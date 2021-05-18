4a0950458b3e04328e411a59730d55b4e008fdca
AustinJohnAaro <austin1472a@gmail.com>
2021-05-18 05:04:34 -0700

    The gang goes on afield trip

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,61 @@
 const router = require('express').Router();
 const  User  = require('../../models');
+const router = require('express').Router();
+const { Post, User } = require('../../models'); 
+
+// get all users
+Post.findAll({
+  attributes: ['id', 'post_url', 'title', 'created_at'],
+  order: [['created_at', 'DESC']], 
+  include: [
+    {
+      model: User,
+      attributes: ['username']
+    }
+  ]
+}) 
+
+router.post('/', (req, res) => {
+  // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+  Post.create({
+    title: req.body.title,
+    post_url: req.body.post_url,
+    user_id: req.body.user_id
+  })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+}); 
+
+router.get('/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'post_url', 'title', 'created_at'],
+    include: [
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+}); 
+
+
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -74,7 +130,51 @@ router.post('/', (req, res) => {
     });
   }); 
 
-        // add comment syntax in front of this line in the .then()
+  router.put('/:id', (req, res) => {
+    Post.update(
+      {
+        title: req.body.title
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }); 
+
+  router.delete('/:id', (req, res) => {
+    Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+
+  // add comment syntax in front of this line in the .then()
 // res.json({ user: dbUserData } 
     
         // Verify user
@@ -135,6 +235,6 @@ router.delete('/:id', (req, res) => {
       });
   }); 
   
-
+  module.exports = router;
 module.exports = router; 
 
