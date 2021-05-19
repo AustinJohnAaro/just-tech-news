018a0a4279c59992081cbed8120b6aaf183e7b74
AustinJohnAaro <austin1472a@gmail.com>
2021-05-19 07:24:17 -0700

    Almost done, I hope!

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var routes = require('./routes');
+require('dotenv').config()
 // import sequelize connection
 var sequelize = require('./config/connection');
 
@@ -11,14 +12,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-sequelize.sync({force: false}).then(() => {
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-  });
 
+
+app.get('/', (req, res) => {
+  res.send(process.env.SECRET_KEY);
 })
-
-
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}.`)
+})
 
 
 
